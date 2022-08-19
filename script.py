e259b9671c1fd73feeefd6da1e7a122a1fde3724
MSDkingdom <84111953+MSDuserbot@users.noreply.github.com>
2022-08-19 11:10:19 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,13 +4,13 @@ class Scripted(object):
  ABOUT_TEXT = """
 ╭────[🔅Rᴇɴᴀᴍᴇʀ Bᴏᴛ🔅]───⍟
 │
-├<b>🤖 Bot Name : <a href='https://t.me/Renamer_teleroid_bot'>Movie Bot</a></b>
+├<b>🤖 Bot Name : <a href='https://t.me/TNFileShareXBot'>Movie Bot</a></b>
 │
-├<b>📢 Channel : <a href='https://t.me/TamilBots'>TamilBots</a></b>
+├<b>📢 Channel : <a href='https://t.me/Tnmovieschat'>TNMovies</a></b>
 │
-├<b>👥 Support Chat : <a href='https://t.me/TamilSupport'>TamilSupport</a></b>
+├<b>👥 Support Chat : <a href='https://t.me/TNmoviechat'>TNMoviechat</a></b>
 │
-├<b>💢 Source : <a href='https://github.com/imsaravanakrish'>Click Here</a></b>
+├<b>💢 Source : <a href='https://github.com/'>Click Here</a></b>
 │
 ├<b>🌐 Server : <a href='https://heroku.com'>Heroku</a></b>
 │
@@ -18,13 +18,13 @@ class Scripted(object):
 │
 ├<b>㊙ Language: <a href='https://www.python.org'>Python 3.9.4</a></b>
 │
-├<b>👨‍💻 Developer : <a href='https://t.me/SaravanaKrish'>✭ Iϻsαi🎭Arⱥ𝖘aภ ✭</a></b>
+├<b>👨‍💻 Developer : <a href='https://t.me/AboutMk'>✭ எம்.எஸ்.டி ✭</a></b>
 │
-├<b>🚸 Powered By : <a href='https://t.me/TamilBots'>TamilBotZ</a></b>
+├<b>🚸 Powered By : <a href='https://t.me/Telelinkgalary'>Tele Link</a></b>
 │
 ╰──────[Thanks 😊]───⍟"""
 
 NEW_TXT = """
 ʜᴇʟʟᴏ {first}\n\nɪ ᴄᴀɴ ꜱᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ꜰɪʟᴇꜱ ɪɴ ꜱᴘᴇᴄɪꜰɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜꜱᴇʀꜱ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ɪᴛ ꜰʀᴏᴍ ꜱᴘᴇᴄɪᴀʟ ʟɪɴᴋ
-ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ » @TamilMovies4K
-ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ ɢʀᴏᴜᴘ » @TamilMoviesChat"""          
+ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ » @TNMoviesChat
+ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ ɢʀᴏᴜᴘ » @TNMovieChat"""          

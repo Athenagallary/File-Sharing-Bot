f43eebce0447f23f170a0d1bf8df8f59d0092585
MSDkingdom <84111953+MSDuserbot@users.noreply.github.com>
2022-08-19 11:06:17 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,30 @@
+class Scripted(object):    
+  
+  
+ ABOUT_TEXT = """
+╭────[🔅Rᴇɴᴀᴍᴇʀ Bᴏᴛ🔅]───⍟
+│
+├<b>🤖 Bot Name : <a href='https://t.me/Renamer_teleroid_bot'>Movie Bot</a></b>
+│
+├<b>📢 Channel : <a href='https://t.me/TamilBots'>TamilBots</a></b>
+│
+├<b>👥 Support Chat : <a href='https://t.me/TamilSupport'>TamilSupport</a></b>
+│
+├<b>💢 Source : <a href='https://github.com/imsaravanakrish'>Click Here</a></b>
+│
+├<b>🌐 Server : <a href='https://heroku.com'>Heroku</a></b>
+│
+├<b>📕 Library : <a href='https://github.com/pyrogram'>Pyrogram 1.2.8</a></b>
+│
+├<b>㊙ Language: <a href='https://www.python.org'>Python 3.9.4</a></b>
+│
+├<b>👨‍💻 Developer : <a href='https://t.me/SaravanaKrish'>✭ Iϻsαi🎭Arⱥ𝖘aภ ✭</a></b>
+│
+├<b>🚸 Powered By : <a href='https://t.me/TamilBots'>TamilBotZ</a></b>
+│
+╰──────[Thanks 😊]───⍟"""
+
+NEW_TXT = """
+ʜᴇʟʟᴏ {first}\n\nɪ ᴄᴀɴ ꜱᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ꜰɪʟᴇꜱ ɪɴ ꜱᴘᴇᴄɪꜰɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜꜱᴇʀꜱ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ɪᴛ ꜰʀᴏᴍ ꜱᴘᴇᴄɪᴀʟ ʟɪɴᴋ
+ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ » @TamilMovies4K
+ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ ɢʀᴏᴜᴘ » @TamilMoviesChat"""          
